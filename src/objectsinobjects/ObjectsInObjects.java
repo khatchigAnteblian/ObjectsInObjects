9dc35fa7e317e3cb971ae5de603ee2ae2e5ecefb
Khatchig Anteblian <Khatchig.Anteblian@student.tdsb.on.ca>
2018-10-02 13:45:18 -0400

    Initial Commit

---
+++ src/objectsinobjects/ObjectsInObjects.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objectsinobjects;
+
+/**
+ *
+ * @author 345983704
+ */
+public class ObjectsInObjects {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
